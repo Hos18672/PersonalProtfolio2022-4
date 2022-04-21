9785172ba293868d2203a3672b71077fbfe98ba8
Reza <hos18672@spengergasse.at>
2022-04-22 01:59:13 +0200

    test mobile keyboard

--- src/js/main.js
+++ src/js/main.js
@@ -142,3 +142,9 @@ window.onclick = function (event) {
     element.classList.remove("modalOpen");
   }
 };
+
+
+document.documentElement.style.setProperty('overflow', 'auto')
+ const metaViewport = document.querySelector('meta[name=viewport]')
+  metaViewport.setAttribute('content', 'height=' + initialHeight + 'px, width=device-width, initial-scale=1.0')
+
