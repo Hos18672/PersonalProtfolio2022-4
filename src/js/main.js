842621e52eecfc69c44ee6af5032f28d944769ff
Reza <hos18672@spengergasse.at>
2022-04-22 02:03:07 +0200

    test mobile keyboard

--- src/js/main.js
+++ src/js/main.js
@@ -143,8 +143,3 @@ window.onclick = function (event) {
   }
 };
 
-
-document.documentElement.style.setProperty('overflow', 'auto')
- const metaViewport = document.querySelector('meta[name=viewport]')
-  metaViewport.setAttribute('content', 'height=' + initialHeight + 'px, width=device-width, initial-scale=1.0')
-
